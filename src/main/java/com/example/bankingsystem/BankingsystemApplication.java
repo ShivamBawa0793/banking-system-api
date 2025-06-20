6da8595558a2a9ce351f798ff43044e8b9c0733b
Shivam Bawa <shivambw18@gamil.com>
2025-06-20 07:22:56 +0530

    test commit

--- src/main/java/com/example/bankingsystem/BankingsystemApplication.java
+++ src/main/java/com/example/bankingsystem/BankingsystemApplication.java
@@ -3,6 +3,7 @@ package com.example.bankingsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Main Class
 @SpringBootApplication
 public class BankingsystemApplication {
 
